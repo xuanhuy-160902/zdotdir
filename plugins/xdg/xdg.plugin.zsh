baf8b167ce164248479a399915c50c913450ff4f
xuanhuy-160902 <n1609.xuanhuy@gmail.com>
2024-05-17 23:42:44 +0700

    Update plugin xdg

--- plugins/xdg/xdg.plugin.zsh
+++ plugins/xdg/xdg.plugin.zsh
@@ -4,17 +4,12 @@
 
 # XDG basedirs
 
-export XDG_CONFIG_HOME=~/.config
-export XDG_CACHE_HOME=~/.cache
-export XDG_DATA_HOME=~/.local/share
-export XDG_STATE_HOME=~/.local/state
-export XDG_RUNTIME_DIR=~/.xdg
-export XDG_PROJECTS_DIR=~/Projects
-
-for _xdgdir in XDG_{CONFIG,CACHE,DATA,STATE}_HOME XDG_RUNTIME_DIR; do
-  [[ -e ${(P)_xdgdir} ]] || mkdir -p ${(P)_xdgdir}
-done
-unset _xdgdir
+#? XDG_CONFIG_HOME=~/.config
+#? XDG_CACHE_HOME=~/.cache
+#? XDG_DATA_HOME=~/.local/share
+#? XDG_STATE_HOME=~/.local/state
+#? XDG_RUNTIME_DIR=~/.xdg
+#? XDG_PROJECTS_DIR=~/Projects
 
 #
 # Shell utils
